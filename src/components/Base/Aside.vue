69c016b15b2e8c4ba6376b7ad50c14f4c4439471
Nemure231 <neruhi5321@gmail.com>
2022-02-22 23:36:29 +0700

    add condition on view

--- src/components/Base/Aside.vue
+++ src/components/Base/Aside.vue
@@ -30,18 +30,22 @@
     <aside class="relative w-full h-auto z-10">
         <div id="hide-sidebar" class="w-full fixed inset-y-0 overflow-y-auto h-full left-0 bg-cyan-900 lg:w-64 md:w-64">
             <div
-                class="sticky flex items-center justify-center flex-row flex-wrap space-x-2 text-2xl font-semibold text-center py-3.5 text-white top-0 backdrop-blur-sm bg-white/25">
+                class="sticky flex items-center justify-center flex-row flex-wrap space-x-6 text-2xl font-semibold text-center py-3.5 text-white top-0 backdrop-blur-sm bg-white/25">
                 
-                <div @click="goto" class="lg:hidden md:hidden cursor-pointer basis-[15%] w-auto h-9 inline-flex items-center justify-center text-2xl font-bold text-white bg-cyan-800 rounded-full">
+                <div @click="goto" :class="posts.length == 1 ? 'order-1' : 'order-0'" 
+                    class="lg:hidden md:hidden cursor-pointer basis-[15%] w-auto h-9 inline-flex items-center justify-center text-2xl font-bold text-white bg-cyan-800 rounded-full">
                     +
                 </div>
-                <router-link @click="goto" to="/" class="basis-1/3">
+                <router-link @click="goto" to="/" class="relative basis-1/3">
+                    <div class="absolute -top-3 lg:-right-4 md:-right-4 sm:right-12 right-4 font-semibold text-sm">
+                        1.0
+                    </div>
                     Cymemo
                 </router-link>
-                <div @click="removeAll"
-                class="cursor-pointer basis-[15%] w-auto h-9  inline-flex items-center justify-center text-xl font-bold text-white bg-red-500 rounded-full">
-                     x
-            </div>
+                <div @click="removeAll" :class="posts.length == 1 ? 'hidden' : 'block'"
+                    class="cursor-pointer basis-[15%] w-auto h-9 inline-flex items-center justify-center text-xl font-bold text-white bg-red-500 rounded-full">
+                    x
+                </div>
             </div>
             <div class="flex flex-nowrap first:hidden hover:bg-cyan-800 justify-between items-center text-white font-medium text-lg"
                 v-for="(post, index) in posts.slice(1)" :key="index">
