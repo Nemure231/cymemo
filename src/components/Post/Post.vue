9af38c58b0b1abaf8a1f6fe4ab8d2605f7904e79
Nemure231 <neruhi5321@gmail.com>
2022-02-23 17:42:29 +0700

    fix screen

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -49,13 +49,17 @@ export default {
 <template>
 
 
-<div class="flex flex-col justify-end w-auto">
-    <div class="lg:ml-64 md:ml-64 ml-0 w-full relative">
-        <div class="absolute top-3 lg:right-72 md:right-72 right-6 text-gray-400 font-medium lg:text-lg md:text-lg text-md">
-            {{valueDateGet}}
-        </div>
+<div class="flex flex-col justify-end w-auto overflow-hidden">
+    <div class="lg:ml-64 md:ml-64 ml-0 w-full">
+        <!-- <div class="relative bg-white"> -->
+            <div class="rounded-3xl absolute bg-cyan-900 inset-x-0 w-max top-[4.5rem] lg:left-[57%] md:left-[60%] left-[44%] p-2 text-white drop-shadow-xl font-medium lg:text-sm md:text-sm text-xs">
+                {{valueDateGet}}
+            </div>
+
+        <!-- </div> -->
+      
         <textarea readonly  placeholder="Description ..."
-        id="edit-post" v-model="valueIsiGet" class="lg:text-xl md:text-xl text-lg px-6 lg:pt-6 md:pt-6 pt-12 w-full h-[550px] resize-none focus:outline-none"
+        id="edit-post" v-model="valueIsiGet" class="lg:text-xl md:text-xl text-lg px-6 pt-12 w-full h-[550px] resize-none focus:outline-none"
         @input="edit(this.$route.params.id)"
         @change="edit(this.$route.params.id)"
         @dblclick="showEdit"
