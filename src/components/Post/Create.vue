575d4fa6ef053029144ddfc347d222a57e76c4ce
Nemure231 <neruhi5321@gmail.com>
2022-02-23 15:34:38 +0700

    fix screen again

--- src/components/Post/Create.vue
+++ src/components/Post/Create.vue
@@ -21,7 +21,7 @@ export default {
     <div class="lg:block md:block block lg:ml-64 md:ml-64 ml-0 w-full">
     <textarea 
         v-model="value" 
-        class="lg:text-xl md:text-xl text-lg p-6 w-full h-[500px] resize-none focus:outline-none"
+        class="sticky top-0 lg:text-xl md:text-xl text-lg p-6 w-full h-[500px] resize-none focus:outline-none"
     placeholder="Isi ..."></textarea>    
     </div>
 </div>
