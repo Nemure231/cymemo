852addaaaeb63363e77ffe4856cdcd86a510bdb3
Nemure231 <neruhi5321@gmail.com>
2022-02-23 16:22:10 +0700

    fix screen

--- src/components/Post/Create.vue
+++ src/components/Post/Create.vue
@@ -18,7 +18,7 @@ export default {
 <template>
 
 <div class="flex justify-end w-auto">
-    <div class="lg:block md:block block sticky top-12 lg:ml-64 md:ml-64 ml-0 w-full">
+    <div class="lg:block md:block block sticky top-20 lg:ml-64 md:ml-64 ml-0 w-full">
     <textarea 
         v-model="value"
         class=" lg:text-xl md:text-xl text-lg px-6 lg:pt-6 md:pt-6 pt-6 w-full h-[500px] resize-none focus:outline-none"
