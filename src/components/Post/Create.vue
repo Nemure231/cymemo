52df327d2abdc7d3ca7591f9c6166aeebeb2324c
Nemure231 <neruhi5321@gmail.com>
2022-02-23 16:45:03 +0700

    fix screen

--- src/components/Post/Create.vue
+++ src/components/Post/Create.vue
@@ -21,7 +21,7 @@ export default {
     <div class="lg:block md:block block lg:ml-64 md:ml-64 ml-0 w-full">
     <textarea 
         v-model="value"
-        class=" lg:text-xl md:text-xl text-lg px-6 lg:pt-6 md:pt-6 pt-40 w-full h-[500px] resize-none focus:outline-none"
+        class=" lg:text-xl md:text-xl text-lg px-6 lg:pt-6 md:pt-6 pt-12 w-full h-[500px] resize-none focus:outline-none"
     placeholder="Description ..."></textarea>    
     </div>
 </div>
