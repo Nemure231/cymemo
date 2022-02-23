d6bdca0dcd88975145d2fa3a12cd59c25a3aefdf
Nemure231 <neruhi5321@gmail.com>
2022-02-23 16:14:34 +0700

    change readme

--- src/components/Post/Create.vue
+++ src/components/Post/Create.vue
@@ -20,8 +20,8 @@ export default {
 <div class="flex justify-end w-auto">
     <div class="lg:block md:block block lg:ml-64 md:ml-64 ml-0 w-full">
     <textarea 
-        v-model="value" 
-        class=" lg:text-xl md:text-xl text-lg p-6 w-full h-[500px] resize-none focus:outline-none"
+        v-model="value"
+        class=" lg:text-xl md:text-xl text-lg px-6 lg:pt-6 md:pt-6 pt-10 w-full h-[500px] resize-none focus:outline-none"
     placeholder="Description ..."></textarea>    
     </div>
 </div>
